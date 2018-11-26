b7fa63a915c084199923863f20f63189db4685a2
Aaron Deane <aaronmdeane@gmail.com>
2018-11-26 11:34:12 -0800

    Updated Grocery List component to render

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+var GroceryList = () => (
+    <div class="container">
+      <h1>A-Aron's Groceries</h1>
+      <ul>
+        <li>Coffee</li>
+        <li>Milk</li>
+        <li>Eggs</li>
+      </ul>
+    </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+
 // TODO
