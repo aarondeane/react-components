9d9cb5631bf6033a5adc512dca857f997737802b
Aaron Deane <aaronmdeane@gmail.com>
2018-11-26 11:41:07 -0800

    Created nested React components for groceries

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var GroceryList = () => (
     <div class="container">
-      <h1>A-Aron's Groceries</h1>
+      <h2>A-Aron's Groceries</h2>
       <ul>
-        <li>Coffee</li>
-        <li>Milk</li>
-        <li>Eggs</li>
+        <Coffee />
+        <Eggs />
       </ul>
+      
     </div>
+);
+
+var Coffee = () => (
+    <div>Coffee</div>
+);
+var Eggs = () => (
+    <div>Eggs</div>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
