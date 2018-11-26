e41f1f52eef6e19b829a5bbf0e9d769980366e40
Aaron Deane <aaronmdeane@gmail.com>
2018-11-26 11:47:17 -0800

    Refactored grocery list using "props"

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-var GroceryList = () => (
+var GroceryList = (props) => (
     <div class="container">
       <h2>A-Aron's Groceries</h2>
-      <ul>
-        <Coffee />
-        <Eggs />
-      </ul>
-      
+      <GroceryListItem groceries={['Coffee', "Eggs"]}  />    
     </div>
 );
 
-var Coffee = () => (
-    <div>Coffee</div>
-);
-var Eggs = () => (
-    <div>Eggs</div>
-);
+var GroceryListItem = (props) => (
+    <ul>
+        <li>{props.groceries[0]}</li>
+        <li>{props.groceries[1]}</li>
+    </ul>
+)
+
+// var Coffee = () => (
+//     <div>Coffee</div>
+// );
+// var Eggs = () => (
+//     <div>Eggs</div>
+// );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
 
